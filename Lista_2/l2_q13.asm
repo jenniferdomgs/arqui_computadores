a8f5ea99fdf547f918eabc21139eb448873d9b47
Jennifer Domingos <99359081+jenniferdomgs@users.noreply.github.com>
2026-04-04 13:33:15 -0300

    questões 13 e 18 - lista 2

--- Lista_2/l2_q13.asm
+++ Lista_2/l2_q13.asm
@@ -11,7 +11,57 @@ main:
 	addi $7, $0, 100
 	addi $8, $0, 1000
 	
-	slt $9, $5, $6 # se for menor que 10
-	slt $9, $5, $7
-	addi $9, $5, $8
+	slt $9, $5, $6  # se for menor que 10, $9 = 1
+	bne $9, $0, menorQ10
+
 	
+	slt $9, $5, $7 # se for menor que 100, $9 = 1
+	bne $9, $0, menorQ100
+	
+	slt $9, $5, $8 # se for menor que 1000, $9 = 1
+	bne $9, $0, menorQ1000
+	
+	add $4, $0, $5 # a partir de 1000 -> imprime normalmente
+	j fim
+	
+menorQ10: # imprime 3 zeros, guarda o número e pula para o fim (onde imprime)
+	add $4, $0, $0
+	addi $2, $0, 1
+	syscall
+	
+	add $4, $0, $0
+	addi $2, $0, 1
+	syscall
+	
+	add $4, $0, $0
+	addi $2, $0, 1
+	syscall
+	
+	add $4, $0, $5
+	j fim
+	
+menorQ100: # imprime 2 zeros, guarda o número e pula para o fim (onde imprime)
+	add $4, $0, $0
+	addi $2, $0, 1
+	syscall
+	
+	add $4, $0, $0
+	addi $2, $0, 1
+	syscall
+	
+	add $4, $0, $5
+	j fim
+	
+menorQ1000: # imprime 0, guarda o número e pula para o fim (onde imprime)
+	add $4, $0, $0
+	addi $2, $0, 1
+	syscall
+	
+	add $4, $0, $5
+	
+fim:
+	addi $2, $0, 1
+	syscall
+	
+	addi $2, $0, 10
+	syscall
---
+++ Lista_2/l2_q18.asm
@@ -0,0 +1,83 @@
+# Faça um programa que receba um inteiro (entre 0 e 999) e imprima o binário correspondente.
+
+.text
+main:
+    addi $2, $0, 5     
+    syscall
+    
+    add $5, $0, $2 # dado auxiliar 
+    
+    # deslocando p/ esquerda para pegar cada bit -> depois faz and com 1 == 1 se for impar e 0 se for par -> imprime bit por bit
+    
+    # bit 9
+    srl $4, $5, 9         
+    andi $4, $4, 1
+           
+    addi $2, $0, 1      
+    syscall
+    
+    # bit 8
+    srl $4, $5, 8
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 7
+    srl $4, $5, 7
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 6
+    srl $4, $5, 6
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 5
+    srl $4, $5, 5
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 4
+    srl $4, $5, 4
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 3
+    srl $4, $5, 3
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 2
+    srl $4, $5, 2
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 1
+    srl $4, $5, 1
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    # bit 0
+    srl $4, $5, 0
+    andi $4, $4, 1
+    
+    addi $2, $0, 1
+    syscall
+    
+    addi $2, $0, 10
+    syscall
